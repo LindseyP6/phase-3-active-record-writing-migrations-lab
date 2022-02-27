0481003151c77cd117d7aa1d9b34d1a92ba8aee6
Lindsey <lindsey.pregent@gmail.com>
2022-02-27 18:53:50 -0500

    commit tree 245f94395349a44bb55274138b7aabbfee12ad95

---
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -0,0 +1,7 @@
+class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[6.1]
+    def change
+      add_column :name, :grade, :integer
+      add_column :name, :birthdate, :string
+    end
+end
+  
